b91c024565b022e4a995ec0a6f7d8199f2ad4105
Javonet <support@javonet.com>
2019-08-14 21:12:24 +0000

    init

---
+++ src/prueba/Class1.java
@@ -0,0 +1,62 @@
+package prueba;
+
+import Common.Activation;
+import static Common.JavonetHelper.Convert;
+import static Common.JavonetHelper.getGetObjectName;
+import static Common.JavonetHelper.getReturnObjectName;
+import static Common.JavonetHelper.ConvertToConcreteInterfaceImplementation;
+import Common.JavonetHelper;
+import Common.MethodTypeAnnotation;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import prueba.*;
+
+public class Class1 {
+  protected NObject javonetHandle;
+
+  public Class1() {
+    try {
+      javonetHandle = Javonet.New("prueba.Class1");
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public Class1(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.String devolver() {
+    try {
+      java.lang.String res = javonetHandle.invoke("devolver");
+      if (res == null) return "";
+      return (java.lang.String) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return "";
+    }
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
